001066b1875710b7f582285193eb9fb0abd745ae
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-09-28 07:05:07 -0400

    init plant submission form (submit unhandled)

--- src/App.js
+++ src/App.js
@@ -9,8 +9,23 @@ import Footer from './components/Footer.js';
 class App extends Component {
   constructor() {
     super();
-    this.state = {  }
+    this.state = {
+      plantName: '',
+      plantNotes: '',
+    }
   }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    console.log('click handled')
+  };
+
+  handleChange = (event) => {
+    this.setState ({
+      [event.target.id]: event.target.value,
+    });
+    console.log('change handled', event.target)
+  };
 
   render() { 
     return (
@@ -18,7 +33,18 @@ class App extends Component {
         <Header />
         <main>
           <h2>your collection</h2>
-          <h2>add a plant</h2>
+          
+          <section className="addNewPlant">
+            <h2>add a plant</h2>
+            <form action='submit'>
+              <label htmlFor='plantName'>plant name:</label>
+              <input type='text' id='plantName' onChange={this.handleChange} value={this.state.plantName} />
+              <label htmlFor='plantNotes'>plant notes:</label>
+              <input type='text' id='plantNotes' onChange={this.handleChange} value={this.state.plantNotes} />
+              <button onClick={this.handleClick}>Submit</button>
+            </form>
+          </section>
+
         </main>
         <Footer />
       </div>
