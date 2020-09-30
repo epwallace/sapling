ef437b92eaad484df8a1fca8884b01e744f8c297
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-09-30 12:56:40 -0400

    Updates

---
+++ precache-manifest.9c4cac6b862c9c6565ffb546c7688c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66975c5462e9e9e8649b83fe377e8e08",
+    "url": "/sapling/index.html"
+  },
+  {
+    "revision": "064f16f03cf645691643",
+    "url": "/sapling/static/css/main.f984b604.chunk.css"
+  },
+  {
+    "revision": "6ec44cc5fabba1fdc7db",
+    "url": "/sapling/static/js/2.7faebfcd.chunk.js"
+  },
+  {
+    "revision": "689e755408df23a6cba0012d32f8c68a",
+    "url": "/sapling/static/js/2.7faebfcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064f16f03cf645691643",
+    "url": "/sapling/static/js/main.7a2af0e4.chunk.js"
+  },
+  {
+    "revision": "7dd38328cef232e0fbcd",
+    "url": "/sapling/static/js/runtime-main.73b09f45.js"
+  },
+  {
+    "revision": "e0c7be968313e4dd5b89bcebdc8dcebf",
+    "url": "/sapling/static/media/flower.e0c7be96.svg"
+  }
+]);
