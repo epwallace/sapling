a82d50236bb39ba6780e350c6257e85226d81248
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-09-30 18:34:09 -0400

    add + style welcome text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,13 @@ class Header extends Component {
         return (
             <header className='wrapper'>
                 <h1>sapling</h1>
+                <p>
+                    Sapling is a simple plant journal that will help you keep tabs on your plant collection.
+                    Add a plant to Sapling and write any notes you find helpful:
+                    care instructions, where you bought it, or anything else.
+                    Your entries will appear in the area below this message.
+                    Happy growing!
+                </p>
             </header>
         );
     }
