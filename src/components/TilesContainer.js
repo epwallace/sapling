5d94a87b5b71cd4265297e6e5bac7e702bdd869b
Evan Wallace <evan.wallace@mail.utoronto.ca>
2020-10-01 09:46:38 -0400

    remove console.log

--- src/components/TilesContainer.js
+++ src/components/TilesContainer.js
@@ -4,7 +4,6 @@ import Tile from './Tile.js';
 const TilesContainer = (props) => {
     const { plants, clickHandler } = props;
     if (plants.length === 0) {
-        console.log('nothing here');
         return (
             <p className="noPlantsMsg">
                 You haven't added any plants yet! Click the button below to get started.
